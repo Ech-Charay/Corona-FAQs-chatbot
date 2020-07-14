1a2a635fe18e9522294ef7a84481c46066401ccb
Mohamed Ech-Charay <echcharaymoh@gmail.com>
2020-07-14 21:28:19 +0100

    Create release-tasks.sh

---
+++ release-tasks.sh
@@ -0,0 +1,4 @@
+wget "https://download.pytorch.org/models/tutorials/4000_checkpoint.tar" 
+python ./cosine_similarity_based_retrieval_chatbot/construct_csv_files
+python ./generative_smart_chatbot/buillding_models
+mkdir -p records/in records/out
