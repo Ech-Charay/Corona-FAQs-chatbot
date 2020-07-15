34400056c3f943de75405cd44ba07eb22fb6ed90
Mohamed Ech-Charay <echcharaymoh@gmail.com>
2020-07-15 20:25:46 +0100

    Update release-tasks.sh

--- release-tasks.sh
+++ release-tasks.sh
@@ -1,5 +1,2 @@
+mkdir -p records/in records/out models
 python -m wget -o "/app/models/4000_checkpoint.tar" "https://download.pytorch.org/models/tutorials/4000_checkpoint.tar"
-python -m spacy download en_core_web_sm
-python ./cosine_similarity_based_retrieval_chatbot/construct_csv_files
-python ./generative_smart_chatbot/buillding_models
-mkdir -p records/in records/out
