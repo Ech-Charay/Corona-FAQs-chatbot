0766cfcdd88afa6f8001078db0ca65016bef6461
Mohamed Ech-Charay <echcharaymoh@gmail.com>
2020-07-15 23:17:43 +0100

    Update release-tasks.sh

--- release-tasks.sh
+++ release-tasks.sh
@@ -1,3 +1,2 @@
 mkdir -p records/in records/out models
 python -m wget -o "/app/models/4000_checkpoint.tar" "https://download.pytorch.org/models/tutorials/4000_checkpoint.tar"
-heroku ps:copy /app/models/4000_checkpoint.tar --app=coronafaqsbot --output=/app/models/4000_checkpoint.tar
