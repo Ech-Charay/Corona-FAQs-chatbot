3978a895476164ff2ab62c7910b624b97572beb1
idamerce <52379531+idamerce@users.noreply.github.com>
2020-07-15 19:46:08 +0100

    Update release-tasks.sh

--- release-tasks.sh
+++ release-tasks.sh
@@ -1,4 +1,4 @@
-wget "https://download.pytorch.org/models/tutorials/4000_checkpoint.tar"
+python -m wget -o "/app/models/4000_checkpoint.tar" "https://download.pytorch.org/models/tutorials/4000_checkpoint.tar"
 python -m spacy download en_core_web_sm
 python ./cosine_similarity_based_retrieval_chatbot/construct_csv_files
 python ./generative_smart_chatbot/buillding_models
