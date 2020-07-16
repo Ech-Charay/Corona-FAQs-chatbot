9c2eb00f646e8c5c6d81de896f60db5f6f804d53
Mohamed Ech-Charay <echcharaymoh@gmail.com>
2020-07-16 06:02:09 +0100

    Update bot_server.py

--- bot_server.py
+++ bot_server.py
@@ -141,7 +141,7 @@ class BotServer:
             engine = gTTS(''+erreur)
             engine.save(os.path.join(self.REC_RES_FOLDER, respfilename))
             list_records.append(respfilename)
-          while os.path.isfile('./path_of_file') == False:
+          while os.path.isfile(self.REC_RES_FOLDER +'/'+ respfilename) == False:
             print("file isn't created yet")
           duration = round(librosa.get_duration(filename= self.REC_RES_FOLDER + '/' + respfilename))
           # Return json file as webhook response 
