4fcd0486cf566e342d08f69eb98d45162f1fc8ab
Mohamed Ech-Charay <echcharaymoh@gmail.com>
2020-07-18 00:53:34 +0100

    Update evaluate_query.py

--- generative_smart_chatbot/evaluate_query.py
+++ generative_smart_chatbot/evaluate_query.py
@@ -9,21 +9,25 @@ MAX_LENGTH = 10  # Maximum sentence length
 device = torch.device("cpu")
 
 def evaluate(searcher, voc, sentence, max_length=MAX_LENGTH):
-    ### Format input sentence as a batch
-    # words -> indexes
-    indexes_batch = [indexesFromSentence(voc, sentence)]
-    # Create lengths tensor
-    lengths = torch.tensor([len(indexes) for indexes in indexes_batch])
-    # Transpose dimensions of batch to match models' expectations
-    input_batch = torch.LongTensor(indexes_batch).transpose(0, 1)
-    # Use appropriate device
-    input_batch = input_batch.to(device)
-    lengths = lengths.to(device)
-    # Decode sentence with searcher
-    tokens, scores = searcher(input_batch, lengths, max_length)
-    # indexes -> words
-    decoded_words = [voc.index2word[token.item()] for token in tokens]
-    return decoded_words
+    try:
+        ### Format input sentence as a batch
+        # words -> indexes
+        indexes_batch = [indexesFromSentence(voc, sentence)]
+        # Create lengths tensor
+        lengths = torch.tensor([len(indexes) for indexes in indexes_batch])
+        # Transpose dimensions of batch to match models' expectations
+        input_batch = torch.LongTensor(indexes_batch).transpose(0, 1)
+        # Use appropriate device
+        input_batch = input_batch.to(device)
+        lengths = lengths.to(device)
+        # Decode sentence with searcher
+        tokens, scores = searcher(input_batch, lengths, max_length)
+        # indexes -> words
+        decoded_words = [voc.index2word[token.item()] for token in tokens]
+        return decoded_words
+    except KeyError:
+        print("Error: Encountered unknown word.")
+        return "I didn't understand you correctly, you may have written a word wrong. Please correct your language!"
 
 # Evaluate inputs from user input (stdin)
 def evaluateInput(searcher, voc,input_sentence):
@@ -46,6 +50,7 @@ def evaluateInput(searcher, voc,input_sentence):
             print('Bot:', ' '.join(output_words))
         except KeyError:
             print("Error: Encountered unknown word.")
+            return "I didn't understand you correctly, you may have written a word wrong. Please correct your language!"
 
 
 
@@ -68,6 +73,8 @@ def evaluateInput2(searcher, voc):
     engine.save('a1.wav') 
   except KeyError:
     print("Error: Encountered unknown word.")
+    return "I didn't understand you correctly, you may have written a word wrong. Please correct your language!"
+
 
 
 
@@ -75,10 +82,14 @@ def evaluateInput2(searcher, voc):
 
 # Normalize input sentence and call evaluate()
 def evaluateExample(sentence, searcher, voc):
-    print("> " + sentence)
-    # Normalize sentence
-    input_sentence = normalizeString(sentence)
-    # Evaluate sentence
-    output_words = evaluate(searcher, voc, input_sentence)
-    output_words[:] = [x for x in output_words if not (x == 'EOS' or x == 'PAD')]
-    print('Bot:', ' '.join(output_words))
+    try:
+        print("> " + sentence)
+        # Normalize sentence
+        input_sentence = normalizeString(sentence)
+        # Evaluate sentence
+        output_words = evaluate(searcher, voc, input_sentence)
+        output_words[:] = [x for x in output_words if not (x == 'EOS' or x == 'PAD')]
+        print('Bot:', ' '.join(output_words))
+    except KeyError:
+        print("Error: Encountered unknown word.")
+        return "I didn't understand you correctly, you may have written a word wrong. Please correct your language!"
