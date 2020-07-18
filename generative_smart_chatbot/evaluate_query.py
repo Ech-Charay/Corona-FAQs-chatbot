9bd5169a6dd75129a68cc6f5ed44b16544aeae3c
idamerce <52379531+idamerce@users.noreply.github.com>
2020-07-18 21:40:04 +0100

    Update evaluate_query.py

--- generative_smart_chatbot/evaluate_query.py
+++ generative_smart_chatbot/evaluate_query.py
@@ -1,7 +1,7 @@
 from .vocabulary import indexesFromSentence, normalizeString
 import torch
 
-from gtts import gTTS #Import Google Text to Speech
+from gtts import gTTS 
 from IPython.display import Audio  
 
 
@@ -10,39 +10,26 @@ device = torch.device("cpu")
 
 def evaluate(searcher, voc, sentence, max_length=MAX_LENGTH):
     try:
-        ### Format input sentence as a batch
-        # words -> indexes
         indexes_batch = [indexesFromSentence(voc, sentence)]
-        # Create lengths tensor
         lengths = torch.tensor([len(indexes) for indexes in indexes_batch])
-        # Transpose dimensions of batch to match models' expectations
         input_batch = torch.LongTensor(indexes_batch).transpose(0, 1)
-        # Use appropriate device
         input_batch = input_batch.to(device)
         lengths = lengths.to(device)
-        # Decode sentence with searcher
         tokens, scores = searcher(input_batch, lengths, max_length)
-        # indexes -> words
         decoded_words = [voc.index2word[token.item()] for token in tokens]
         return decoded_words
     except KeyError:
         print("Error: Encountered unknown word.")
-        return ["I", "didn't", "understand","you", "correctly,", "you", "may", "have", "written", "a", "word", "wrong.", "Please", "correct", "your", "language!"]
+        raise Exception("Erreur:evaluate")
 
 # Evaluate inputs from user input (stdin)
 def evaluateInput(searcher, voc,input_sentence):
     #input_sentence = ''
     while(1):
         try:
-            # Get input sentence
-            #input_sentence = input('> ')
-            # Check if it is quit case
-            #if input_sentence == 'q' or input_sentence == 'quit': break
-            # Normalize sentence
+
             input_sentence = normalizeString(input_sentence)
-            # Evaluate sentence
             output_words = evaluate(searcher, voc, input_sentence)
-            # Format and print response sentence
             output_words[:] = [x for x in output_words if not (x == 'EOS' or x == 'PAD')]
             engine = gTTS(''.join(output_words))  
             engine.save('a1.wav') 
@@ -50,46 +37,6 @@ def evaluateInput(searcher, voc,input_sentence):
             print('Bot:', ' '.join(output_words))
         except KeyError:
             print("Error: Encountered unknown word.")
-            return ["I", "didn't", "understand","you", "correctly,", "you", "may", "have", "written", "a", "word", "wrong.", "Please", "correct", "your", "language!"]
+            raise Exception("Erreur:evaluateInput")
 
 
-
-# Evaluate inputs from user input (stdin)
-def evaluateInput2(searcher, voc):
-  input_sentence = ''
-  try:
-    # Get input sentence
-    input_sentence = input('> ')
-    # Check if it is quit case
-    # Normalize sentence
-    input_sentence = normalizeString(input_sentence)
-    # Evaluate sentence
-    output_words = evaluate(searcher, voc, input_sentence)
-    # Format and print response sentence
-    output_words[:] = [x for x in output_words if not (x == 'EOS' or x == 'PAD')]
-    print('Bot:', ' '.join(output_words))
-    bot_answer=' '.join(output_words)
-    engine = gTTS(''+bot_answer)  
-    engine.save('a1.wav') 
-  except KeyError:
-    print("Error: Encountered unknown word.")
-    return ["I", "didn't", "understand","you", "correctly,", "you", "may", "have", "written", "a", "word", "wrong.", "Please", "correct", "your", "language!"]
-
-
-
-
-
-
-# Normalize input sentence and call evaluate()
-def evaluateExample(sentence, searcher, voc):
-    try:
-        print("> " + sentence)
-        # Normalize sentence
-        input_sentence = normalizeString(sentence)
-        # Evaluate sentence
-        output_words = evaluate(searcher, voc, input_sentence)
-        output_words[:] = [x for x in output_words if not (x == 'EOS' or x == 'PAD')]
-        print('Bot:', ' '.join(output_words))
-    except KeyError:
-        print("Error: Encountered unknown word.")
-        return ["I", "didn't", "understand","you", "correctly,", "you", "may", "have", "written", "a", "word", "wrong.", "Please", "correct", "your", "language!"]
